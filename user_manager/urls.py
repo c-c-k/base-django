87c91c68afab9e17cbf200c92d25c5060e3f5f6e
c-c-k <c-c-k@nym.hush.com>
2023-02-24 13:00:21 +0200

    Fixed root url redirect

--- user_manager/urls.py
+++ user_manager/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, reverse_lazy
+from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url=reverse_lazy('default:login'))),
+    path('', RedirectView.as_view(url='default/')),
     path('default/', include('default.urls')),
     path('admin/', admin.site.urls),
 ]
