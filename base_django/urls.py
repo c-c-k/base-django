fc9be93219e522ac5f724d19e614601b3624ddc4
c-c-k <c-c-k@nym.hush.com>
2023-02-25 10:39:27 +0200

    Added logout

--- base_django/urls.py
+++ base_django/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='accounts/')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='accounts/')),
 ]
